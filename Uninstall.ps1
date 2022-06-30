c8ed992073f26fc04354775d357f399c7368876c
Professional <shubhojeetde2@gmail.com>
2022-06-30 16:15:33 +0530

    Fix uninstall script

--- Uninstall.ps1
+++ Uninstall.ps1
@@ -1,4 +1,4 @@
-Get-Process * | Where-Object {$_.mainWindowTitle -match "Cursor Inverter" -or $_.Path -match "CursorInverter"} | Stop-Process -Force
+Get-Process * | Where-Object { (Get-CimInstance Win32_Process -Filter "ProcessId = $($_.Id)").CommandLine -match "CursorInverter" } | Stop-Process -Force
 Unregister-ScheduledTask -TaskName "CursorInverter"
 Remove-Item -Path "$Env:ProgramFiles\CursorInverter.ps1" -Force
 Write-Output "CursorInverter has been uninstalled!"
