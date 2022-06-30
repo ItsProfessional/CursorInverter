70de2126a6b5d29580df6fc0b377d4852954f00a
Professional <shubhojeetde2@gmail.com>
2022-06-30 15:30:03 +0530

    Elevate install and uninstall scripts if not already elevated

--- Install.ps1
+++ Install.ps1
@@ -1,11 +1,19 @@
+if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    Start-Process -FilePath $((Get-Process -Id $PID).Path) -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`" `"$($MyInvocation.MyCommand.UnboundArguments)`""
+    Exit
+}
+
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ItsProfessional/CursorInverter/main/CursorInverter.ps1" -OutFile "$Env:ProgramFiles\CursorInverter.ps1"
+
 $FilePath = "$Env:SystemRoot\System32\mshta.exe"
 $Argument = @'
 vbscript:Execute("CreateObject(""Wscript.Shell"").Run ""powershell -NoLogo -Command """"& 'C:\Program Files\CursorInverter.ps1'"""""", 0 : window.close")
 '@
+
 $Trigger = New-ScheduledTaskTrigger -AtLogon
 $Action = New-ScheduledTaskAction -Execute $FilePath -Argument $Argument
 $Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 Register-ScheduledTask -TaskName "CursorInverter" -Trigger $Trigger -Action $Action -RunLevel Highest -Settings $Settings | Out-Null
+
 Start-Process -FilePath $FilePath -ArgumentList $Argument
 Write-Output "CursorInverter has been installed!"
--- Uninstall.ps1
+++ Uninstall.ps1
@@ -1,3 +1,8 @@
+if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    Start-Process -FilePath $((Get-Process -Id $PID).Path) -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`" `"$($MyInvocation.MyCommand.UnboundArguments)`""
+    Exit
+}
+
 Get-Process * | Where-Object {$_.mainWindowTitle -match "Cursor Inverter" -or $_.Path -match "CursorInverter"} | Stop-Process -Force
 Unregister-ScheduledTask -TaskName "CursorInverter"
 Remove-Item -Path "$Env:ProgramFiles\CursorInverter.ps1" -Force
