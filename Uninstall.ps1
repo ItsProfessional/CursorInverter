15d16da45f418c34b3ff4708154f9dd2e34376f4
Professional <shubhojeetde2@gmail.com>
2022-06-30 16:30:52 +0530

    Call cmd to delete the file because removeitem keeps showing confirmations

--- Uninstall.ps1
+++ Uninstall.ps1
@@ -1,4 +1,5 @@
 Get-Process * | Where-Object { (Get-CimInstance Win32_Process -Filter "ProcessId = $($_.Id)").CommandLine -match "CursorInverter" } | Stop-Process -Force
 Unregister-ScheduledTask -TaskName "CursorInverter"
-Remove-Item -Path "$Env:ProgramFiles\CursorInverter.ps1" -Force -Confirm:$false
+# Remove-Item -Path "$Env:ProgramFiles\CursorInverter.ps1" -Force -Confirm:$false
+cmd /c 'del /f "%ProgramFiles%\CursorInverter.ps1"'
 Write-Output "CursorInverter has been uninstalled!"
